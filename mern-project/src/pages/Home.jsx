5dd0141edec0e7d4ad443c8e614e0f83312ffabe
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2025-10-04 11:48:40 +0530

    initial commit

--- mern-project/src/pages/Home.jsx
+++ mern-project/src/pages/Home.jsx
@@ -43,15 +43,15 @@ export default function Home() {
   return (
     <div>
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
-        <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>Find your next <span className='text-slate-500'>perfect</span> 
+        <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl capitalize'>Find your next <span className='text-slate-500'>perfect</span> 
         <br />place with ease</h1>
-        <div className="text-gray-700 text-xs sm:text-sm">
+        <div className="text-gray-700 text-xs sm:text-sm font-semibold">
           Real Estate listings at your fingertips. We make it easy to find your dream home.
           <br />
           Discover homes, apartments, and more.
           <br />
         </div>
-        <Link to={"/search"} className='text-xs sm:text-sm text-blue-800 font-bold hover:underline'>
+        <Link to={"/search"} className='text-xs sm:text-sm text-blue-600 font-semibold hover:underline'>
           Let's get started...
         </Link>
       </div>
