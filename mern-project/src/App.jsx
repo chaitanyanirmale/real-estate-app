ab6b3af8940d01d9fd497d54b72bb3d22a2cb723
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-08-13 22:09:28 +0530

    Contact page route added

--- mern-project/src/App.jsx
+++ mern-project/src/App.jsx
@@ -11,6 +11,7 @@ import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
 import Search from './pages/Search';
+import Contact from './pages/Contact';
 
 export default function App() {
   return (
@@ -22,6 +23,7 @@ export default function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/about" element={<About />} />
         <Route path="/search" element={<Search />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/listing/:listingId" element={<Listing />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
